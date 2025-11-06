4f619aaed061b02428de2a9b53df0b87d01ce5cb
ImronSatrioPurnomo <satriogames2223@gmail.com>
2025-11-06 16:41:53 +0700

    menambahkan function dataApp di dalam folder Navigasi.kt dan isinya, serta menambahkan scaffold

--- app/src/main/java/com/example/navigasiku/Navigasi.kt
+++ app/src/main/java/com/example/navigasiku/Navigasi.kt
@@ -1,7 +1,20 @@
 package com.example.navigasiku
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
 enum class Navigasi {
     Formulirku,
     Detail
 }
 
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+){
+    Scaffold { isiRuang ->
+
+    }
+}
