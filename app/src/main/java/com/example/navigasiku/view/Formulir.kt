3112b10335f76e17644cac35099c41b6065bee63
ImronSatrioPurnomo <satriogames2223@gmail.com>
2025-11-06 15:35:08 +0700

    menambahkan scaffold di dalam function FormIsian

---
+++ app/src/main/java/com/example/navigasiku/view/Formulir.kt
@@ -0,0 +1,25 @@
+package com.example.navigasiku.view
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+
+@Composable
+fun FormIsian(
+    jenisK:List<String> = listOf("Laki-laki","Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+){
+    Scaffold (modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = {Text(text = stringResource(id = R.string.home),
+                    color = Color.White)},
+                colors = TopAppBarDefaults.TopAppBarColors
+                    (containerColor = colorResource(id = R.color.teal_700))
+            )
+        })
+}
